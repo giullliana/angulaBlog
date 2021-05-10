01d5207459b9a720cc25d18aad6434589a2972ca
Giulliana Munhoz <giulli_munhoz@hotmail.com>
2021-05-10 17:32:47 -0300

    Implementação do post e get de postagens

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -23,6 +23,11 @@ export class AuthService {
     return this.http.post<User>("http://localhost:8080/usuarios/cadastrar", user)
 
   }
+
+  getByIdUser(id: number): Observable<User>{
+    return this.http.get<User>(`http://localhost:8080/usuarios/${id}`)
+  }
+
   logado() {
     let ok: boolean = false
 
